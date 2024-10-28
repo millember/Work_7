53407f19f234fca4c7fadbeed294ac84184ca2c4
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-10-28 21:15:39 +0500

    25/1/3

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'users',
     'vehicle',
+    'rest_framework_simplejwt',
 
 ]
 
@@ -128,3 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
